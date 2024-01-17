f6c441c24ea28e4c04528669292b3cd051ccb6b6
PlanetJack <122494246+PlanetJack@users.noreply.github.com>
2024-01-17 16:50:23 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -14,6 +14,16 @@ document.addEventListener('scroll', () => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
-    console.log(1 - window.screenY / homeHeight);
-    home.style.opacity = 1 - window.screenY / homeHeight; 
+    home.style.opacity = 0.9 - window.scrollY / homeHeight; 
+});
+
+
+// Arrow up 버튼을 아래로 스크롤시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up'); 
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 0.9;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
 });
