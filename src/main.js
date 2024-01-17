7fab7decd4574e9574f1a00d03e1b9fe2dd1e149
PlanetJack <122494246+PlanetJack@users.noreply.github.com>
2024-01-17 11:53:03 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
@@ -7,4 +8,12 @@ document.addEventListener('scroll', () => {
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+// Home Section 을 아래로 스크롤시 점점 투명하게 처리함.
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    console.log(1 - window.screenY / homeHeight);
+    home.style.opacity = 1 - window.screenY / homeHeight; 
 });
