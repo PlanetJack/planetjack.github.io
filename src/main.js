562c9a2f22b08d3da9ea77bcc795caae8e0ce2f5
PlanetJack <122494246+PlanetJack@users.noreply.github.com>
2024-01-17 11:26:38 +0900

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  }
+});
